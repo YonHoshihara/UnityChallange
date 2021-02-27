be5de33e2cbae4d251c7e1b36f95e1f86e753d11
YonHoshihara <hoshiharyon@gmail.com>
2021-02-27 00:03:30 -0300

    Add grid generator script

---
+++ Assets/Scripts/Scene/GridController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tile;
+    [SerializeField] private float cellSize;
+    [SerializeField] private Vector2 gridSize;
+    public float [][] gridCoordenates;
+
+
+
+    void Start()
+    {
+
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            for (int j = 0; j < gridSize.y; j++)
+            {
+                Instantiate(tile, new Vector3( i*cellSize, 0, j*cellSize), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
